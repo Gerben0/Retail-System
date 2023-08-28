ced64eb959b403593ec7af2c3589e5396d6eb896
Gerben0 <smits.gerben@live.nl>
2023-08-28 15:40:45 +0200

    Added sales table

--- dbimport/import.sql
+++ dbimport/import.sql
@@ -8,13 +8,13 @@ USE retail_system;
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `barcode` int(13) DEFAULT NULL,
+  `barcode` int(13) NOT NULL,
   `inventory` int(10) NOT NULL,
-  `retail_price` decimal(2,2) DEFAULT NULL,
+  `retail_price` decimal(2,2) NOT NULL,
   `default_cost` decimal(2,2) DEFAULT NULL,
   `tax_rate` decimal(5,2) DEFAULT NULL,
   `description` varchar(1500) DEFAULT NULL,
-  `vendor` varchar(100) NOT NULL,
+  `vendor` varchar(100) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `items`
@@ -32,10 +32,10 @@ CREATE TABLE `customers` (
   `address` varchar(100) DEFAULT NULL,
   `zip_code` varchar(10) DEFAULT NULL,
   `city` varchar(30) DEFAULT NULL,
-  `country` varchar(30) NOT NULL,
+  `country` varchar(30) DEFAULT NULL,
   `email_address` varchar(100) NOT NULL,
-  `phone_number` varchar(30) NOT NULL,
-  `notes` varchar(1500) NOT NULL,
+  `phone_number` varchar(30) DEFAULT NULL,
+  `notes` varchar(1500) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `customers`
@@ -44,6 +44,18 @@ ALTER TABLE `customers`
 ALTER TABLE `customers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `products` varchar(1500) NOT NULL,
+  `sale_amount` decimal(2,2) NOT NULL,
+  `customer` varchar(100) NOT NULL,
+  `sale_date` datetime DEFAULT '2055-01-01 12:00:00',
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 COMMIT;
